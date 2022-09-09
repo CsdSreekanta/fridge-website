ef1b91d114f9c79c340619599a594cb522d3734e
HP <sreekanta.csd@gmail.com>
2022-09-09 11:46:34 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Blogs from './components/Blogs/Blogs';
+import Dashboard from './components/Dashboard/Dashboard';
+import Home from './components/Home/Home';
+import Review from './components/Review/Review'
+import About from './components/About/About';
 import Navbar from './Navbar/Navbar';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
       <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/review" element={<Review></Review>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path= "/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path ="*" element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
