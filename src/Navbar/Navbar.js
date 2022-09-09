99aa80c74e0a725b645c7eb4563b6e5323ef1764
HP <sreekanta.csd@gmail.com>
2022-09-09 13:01:31 +0600

    headline and picture added

--- src/Navbar/Navbar.js
+++ src/Navbar/Navbar.js
@@ -1,16 +1,43 @@
-import React from 'react';
-import './Navbar.css'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 const navbar = () => {
-    return (
-        <div className='nav bg-blue-600 p-4 shadow-lg'>
-            <a href="">Home</a>
-            <a href="">Review</a>
-            <a href="">Dashboard</a>
-            <a href="">Blogs</a>
-            <a href="">About</a>
-        </div>
-    );
+  return (
+    <div className="nav bg-blue-600 p-4 shadow-lg">
+      <NavLink
+        to="/"
+        className={({ isActive }) => (isActive ? "active" : "inactive")}
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/review"
+        className={({ isActive }) => (isActive ? "active" : "inactive")}
+      >
+      Review
+      </NavLink>
+      <NavLink
+        to="/dashboard"
+        className={({ isActive }) => (isActive ? "active" : "inactive")}
+      >
+        Dashboard
+      </NavLink>
+      <NavLink
+        to="/blogs"
+        className={({ isActive }) => (isActive ? "active" : "inactive")}
+      >
+        Blogs
+      </NavLink>
+      <NavLink
+        to="/About"
+        className={({ isActive }) => (isActive ? "active" : "inactive")}
+      >
+       About
+      </NavLink>
+    
+    </div>
+  );
 };
 
 export default navbar;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import walton from '../images/walton.jpg'
 
 const Home = () => {
     return (
-        <div>
-            <h1>This is home</h1>
+       <div>
+         <div className='grid grid-cols-1 mt-5 p-4 md:grid-cols-2'>
+            <div className="mt-32 sm:order-last border-4">
+                <h1 className='text-6xl font-bold font-mono text-orange-500'>Decs Technology Retains Natural Freshness</h1>
+                <p className='text-xl mt-6 font-bold font-serif'>Welcome to an experience of freshness. WALTON Direct Cool Refrigerators are equipped with 100% copper condensers and Nano technology to take care of your evolving needs and at the same time, maintain the quality of the food that you preserve. Our simple and contemporary designs will surely complement your lifestyle.</p>
+                <button className='mt-10 font-bold text-xl text-white bg-orange-500 px-8 py-3 rounded shadow-lg'>For Details</button>
+            </div>
+            <div className =" md:order-last border-4">
+            <img className='h-screen pl-28' src={walton} alt="" />
+            </div>
         </div>
+        <div>
+            <h1 className='text-center text-3xl font-bold text-orange-500 p-4'> Customer Review (3)</h1>
+        </div>
+       </div>
+        
     );
 };
 
