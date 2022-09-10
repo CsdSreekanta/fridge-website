b1766ef69b38c0c10f87e3c93f264d53546cbcaf
HP <sreekanta.csd@gmail.com>
2022-09-10 08:34:55 +0600

    border updated

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -17,18 +17,18 @@ const Home = () => {
     return (
        <div>
          <div className='grid grid-cols-1 mt-5 p-4 md:grid-cols-2'>
-            <div className="mt-32 sm:order-last border-4">
+            <div className="mt-32 sm:order-last ">
                 <h1 className='text-6xl font-bold font-mono text-orange-500'>Decs Technology Retains Natural Freshness</h1>
                 <p className='text-xl mt-6 font-bold font-serif'>Welcome to an experience of freshness. WALTON Direct Cool Refrigerators are equipped with 100% copper condensers and Nano technology to take care of your evolving needs and at the same time, maintain the quality of the food that you preserve. Our simple and contemporary designs will surely complement your lifestyle.</p>
                 <button className='mt-10 font-bold text-xl text-white bg-orange-500 px-8 py-3 rounded shadow-lg'>For Details</button>
             </div>
-            <div className =" md:order-last border-4">
+            <div className =" md:order-last">
             <img className='h-screen pl-28' src={walton} alt="" />
             </div>
         </div>
         <div>
             <h1 className='text-center text-3xl font-bold text-orange-500 p-4'> Customer Review (3)</h1>
-           <div className='border-4 mt-12 p-3 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 '>
+           <div className='mt-12 p-3 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 '>
          {
           products.slice(0, 3).map(product=><Details key={product.id} product={product}></Details>)
             
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -6,7 +6,7 @@ const Review = () => {
     const [products, setProducts] = useProducts();
     return (
         <div>
-             <div className='border-4 mt-12 p-3 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 '>
+             <div className=' mt-12 p-3 grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-4 '>
          {
             products.map(product=><Details key={product.id} product={product}></Details>)
          }
